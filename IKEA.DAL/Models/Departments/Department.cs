8e7cf26af4e926dcfab188b2245769dded392dc4
Lojain <logyemad2125@gmail.com>
2025-03-26 07:31:17 +0200

    02-Department Module With Configurations

---
+++ IKEA.DAL/Models/Departments/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Models.Departments
+{
+    public class Department: ModelBase
+    {
+        //data annotation will be added in interface in views , dtos , in front end
+        public string Name { get; set; } = null!;   //as not equal null
+
+        public string Code { get; set; } = null!;
+
+        public string? Description { get; set; }   //nullable , can be passed or not
+
+        public DateOnly CreationDate { get; set; }    //this represent the date of create the department and this for user , for business role and endUser
+
+    }
+
+}
---
+++ IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IKEA.DAL.Models.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Persistance.Data
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        //to apply configurations made using fluent api
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());           //assembly is mean the current project   //search for any ine how implement the IEntityTypeConfiguration  , to can implement the configurations
+        }
+        public DbSet<Department> Departments { get; set; }   //this we the table be ready
+
+
+    }
+}
