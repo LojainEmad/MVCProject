d76f466d62fe8377b7ad4cd67a045038155a8d7f
Lojain <logyemad2125@gmail.com>
2025-04-15 08:33:29 +0200

    Final session5

--- IKEA.PL/Program.cs
+++ IKEA.PL/Program.cs
@@ -53,6 +53,7 @@ namespace IKEA.PL
             #region Configure Pipelines (MiddleWares)
             var app = builder.Build();
 
+
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
