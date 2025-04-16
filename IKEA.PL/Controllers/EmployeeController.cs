51b69df5083bf9f0b8f0a0fb2650635c2135071a
Lojain <logyemad2125@gmail.com>
2025-04-16 21:06:34 +0200

    02-Employee Create

--- IKEA.PL/Controllers/EmployeeController.cs
+++ IKEA.PL/Controllers/EmployeeController.cs
@@ -1,4 +1,7 @@
-﻿using IKEA.BLL.Services.EmployeeServices;
+﻿using IKEA.BLL.Dto_s.Departments;
+using IKEA.BLL.Dto_s.Employees;
+using IKEA.BLL.Services.DepartmentServices;
+using IKEA.BLL.Services.EmployeeServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IKEA.PL.Controllers
@@ -25,6 +28,60 @@ namespace IKEA.PL.Controllers
             var Employees = employeeServices.GetAllEmployees();
 
             return View(Employees);
+        }
+        #endregion
+
+        #region Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatedEmployeeDto EmployeeDto)
+        {
+            //ServerSide Validation
+            if (!ModelState.IsValid)
+            {
+                return View(EmployeeDto);
+            }
+            var Message = string.Empty;
+
+
+            try
+            {
+                var Result = employeeServices.CreateEmployee(EmployeeDto);
+                if (Result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    Message = "Employee is not Created";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //1-Log Exception Kestral (not appear for enduser)
+                logger.LogError(ex, ex.Message);
+
+                //2-Set Default Message User
+                if (environment.IsDevelopment())
+                    Message = ex.Message;
+                else
+                {
+                    Message = "An Error affects at the Creation Operator";
+
+                }
+
+            }
+            ModelState.AddModelError(string.Empty, Message);
+            return View(EmployeeDto);
+
+
+            //return View();
+
+
         }
         #endregion
 
